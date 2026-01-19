e6cfe9db86bab0da55e0c0ab237d237ce5c3616b
Varun Girimaji <vgirimaji@gmail.com>
2026-01-19 08:29:20 -0600

    Added raw_prompt that does not use or add to history

--- gem_connection.py
+++ gem_connection.py
@@ -8,13 +8,30 @@ class GeminiConnection:
         self.headers = {'x-goog-api-key': self.key, 'Content-Type': 'application/json'}
         self.history = []
 
+    # Don't add to history, just prompt.
+    def raw_prompt(self, p):
+        jsondata = {"contents":{"role":"user","parts":[{"text":p}]}}
+        txt = self._run_prompt(jsondata)
+
+        return txt
+
+    # Normal "chat"
     def prompt(self, p):
         self.history.append({"role":"user","parts":[{"text":p}]})
         jsondata = {"contents": self.history}
+
+        txt = self._run_prompt(jsondata)
+
+        self.history.append({"role":"model","parts":[{"text":txt["text"]}]})
+
+        return txt
+
+    # Internal, don't use this from outside
+    def _run_prompt(self, jsondata):
         r = requests.post(self.url, data=json.dumps(jsondata), headers=self.headers)
         if r.status_code != 200:
             print(r.text)
-        txt = json.loads(r.text)['candidates'][0]['content']['parts'][0]
-        self.history.append({"role":"model","parts":[{"text":txt["text"]}]})
-        return txt
 
+        txt = json.loads(r.text)['candidates'][0]['content']['parts'][0]
+
+        return txt
